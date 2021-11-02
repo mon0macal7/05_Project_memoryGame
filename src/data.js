4d93c6c2e96a022a122e1a4908ccefba5de3184c
mon0macal7 <monhdz07@gmail.com>
2021-11-02 10:27:10 -0600

    Cambios en data js. y CSS

--- src/data.js
+++ src/data.js
@@ -1,2 +1,30 @@
 //Exportar función para insertar imagen
 export let getPaintCards = () => {};
+//traer data del .json local
+fetch("../data/memory.json")
+  .then((response) => response.json())
+  .then((data) => iterarCards.json(data))
+  .catch((error) => console.log(error));
+
+//función para insertar tarjetas
+
+let artCards = document.getElementById("memoCard");
+
+// variable para el área de tarjetas
+let iterarCards = (data) => {
+  // iterar el objeto
+  console.log(data.memory_game);
+  for (const card of data.memory_Game) {
+    // iterar dinámicamente para entrar al objeto
+    console.log(card.name); // consologear la entrada al objeto
+    artCards.innerHTML +=
+      // imprimir en HTML
+      `<div class="card-container">
+            <div class="card-portrait">
+                <div class="card-cover"></div>
+                <div class="card-back">back: ${card.card_image}</div>
+            </div>
+        </div>
+        `;
+  }
+};
