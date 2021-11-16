bc54ad796fae6070f5388f83118e9a45e820d993
mon0macal7 <monhdz07@gmail.com>
2021-11-16 17:26:29 -0600

    limpie tablero y realice cambios app JS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,17 @@ document.getElementById("mainPage").hidden = true;
 //Acceso con botón play
 
 let jugar = document.getElementById("play");
-jugar.addEventListener("click", function () {
+jugar.addEventListener("click", () => {
   document.getElementById("screenOne").hidden = true;
   document.getElementById("mainPage").hidden = false;
 });
+
+//funcion pintar nombres de jugadores de screen one a main screen
+let jugadorUno = document.getElementById("name1");
+jugadorUno.innerHTML = "player1" + document.getElementById("name1").value + ":";
+let jugadorDos = document.getElementById("name2");
+jugadorDos.innerHTML = "player2" + document.getElementById("name2").value + ":";
+
+//traer tarjetas y darles visibilidad
+/*const reinicio = document.querySelecctorAll("card-portrait");
+reinicio.visibility = "";*/
