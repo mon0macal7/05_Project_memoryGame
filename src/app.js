3d780f1207c0e0d0027ac765536a80c6359c2e2e
mon0macal7 <monhdz07@gmail.com>
2021-11-16 13:39:11 -0600

    add screen one

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 //Esconde la pantalla 1
 
 document.getElementById("screenOne").hidden = false;
-document.getElementById("screenTwo").hidden = true;
+document.getElementById("mainPage").hidden = true;
 
 //Acceso con botón play
 
 let jugar = document.getElementById("play");
 jugar.addEventListener("click", function () {
   document.getElementById("screenOne").hidden = true;
-  document.getElementById("screenTwo").hidden = false;
+  document.getElementById("mainPage").hidden = false;
 });
